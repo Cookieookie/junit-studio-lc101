1789b00cd9ddcbc533acf5920c61606f05bcff05
Cookie Van <cookievan03@gmail.com>
2023-09-06 19:31:07 -0500

    finished tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +9,78 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+//    @Test
+//    public void emptyTest() {
+//        assertEquals(true, true);
+//    }
+
+    //Test 1
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    //Test 2
+    @Test
+    public void emptyBackwardsBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
 
+    //Test 3
+    @Test
+    public void oneOpeningBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    //Test 4
+    @Test
+    public void oneClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+
+//Test 5
+    @Test
+    public void oddNumberOfBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode]["));
+    }
+//Test 6
+    @Test
+    public void unbalancedBracketsReturnFalse() {
+    assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+}
+//Test 7
+    @Test
+    public void multipleNestingBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]] [[[[]]]]"));
+    }
+
+//Test 8
+    @Test
+    public void emptyBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+//Test 9
+    @Test
+    public void multipleClosingBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Lau[n]chc[ode]"));
+    }
+
+//Test 10
+@Test
+public void multipleOpenBracketsReturnFalse() {
+    assertFalse(BalancedBrackets.hasBalancedBrackets("][Lau]n[chc]ode["));
+}
+
+//Test 11
+@Test
+    public void openBracketsFlankingClosingBracketsReturnFalse() {
+    assertFalse(BalancedBrackets.hasBalancedBrackets("][]["));
+}
+//Test 12
+
+    @Test
+    public void openBracketsWithinOnePairOfClosedBracketsReturnsTrue() {
+    assertTrue(BalancedBrackets.hasBalancedBrackets("[]]]]][[[[[]"));
+}
 }
